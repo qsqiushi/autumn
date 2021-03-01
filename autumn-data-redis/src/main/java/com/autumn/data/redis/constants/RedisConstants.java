126c8abaac1993a61373e7ad37e81c629aebc272
qiushi <762999521@qq.com>
2021-03-01 14:27:11 +0800

    redisson 引入

---
+++ autumn-data-redis/src/main/java/com/autumn/data/redis/constants/RedisConstants.java
@@ -0,0 +1,22 @@
+package com.autumn.data.redis.constants;
+
+/**
+ * @program: autumn
+ * @description:
+ * @author: qius
+ * @create: 2021-03-01:14:18
+ */
+public class RedisConstants {
+
+  public static final String DEFAULT_DEFENSE_VALUE = "NULL";
+
+  public static final Long CACHE_ONE_MINUTE = 60L;
+
+  public static final Long CACHE_ONE_HOUR = 3600L;
+
+  public static final Long CACHE_ONE_DAY = 86400L;
+
+  public static final Long CACHE_ONE_MONTH = 18144000L;
+
+  public static final Long CACHE_ONE_YEAR = 31536000L;
+}
